7411be697ae3e346953dce0d27ce05289bdf7aac
FrancescoPranzo <52277710+FrancescoPranzo@users.noreply.github.com>
2020-08-19 15:38:40 +0200

    Update main.py

--- UCI/Computer Hardware Data Set/main.py
+++ UCI/Computer Hardware Data Set/main.py
@@ -2,17 +2,39 @@ import pandas as pd
 import numpy as np
 
 
-machine = pd.read_csv("machine.data", names=["vendor name", "Model Name", "MYCT", "MMIN", "MMAX", "CACH", "CHMIN", "CHMAX", "PRP", "ERP"])
-machine.head()
+import pandas as pd
+import numpy as np
 
-machine.head()
+
+machine = pd.read_csv("machine.data", names=["vendor name", "Model Name", "MYCT", "MMIN", "MMAX", "CACH", "CHMIN", "CHMAX", "PRP", "ERP"])
+
 machine.info()
 
 #see Correlation between data
-machine.corr()
+print(machine.corr())
 
-x = machine.drop("ERP", axis=1).values
+x = machine[["MMIN", "MMAX"]].values
 y = machine["ERP"].values
 
 x.shape
 y.shape
+
+
+from sklearn.model_selection import train_test_split
+
+xTrain, xTest, yTrain, yTest = train_test_split(x,y, test_size=0.3, random_state=0)
+
+#creating the model
+from sklearn.linear_model import LinearRegression
+
+ll = LinearRegression()
+
+ll.fit(xTrain, yTrain)
+
+yPred = ll.predict(xTest)
+
+#valutate the model
+from sklearn.metrics import mean_squared_error, r2_score
+
+print(mean_squared_error(yTest, yPred ))
+r2_score(yTest, yPred
